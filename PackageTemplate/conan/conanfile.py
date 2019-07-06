f24a5721da140c39670f53bbfc329579b724bb09
saitama <sitkowskis@interia.pl>
2019-07-06 22:24:18 +0200

    refactor

--- PackageTemplate/conan/conanfile.py
+++ PackageTemplate/conan/conanfile.py
@@ -1,31 +1,32 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
+import os
 
-
-class TestpackageConan(ConanFile):
-    name = "testPackage"
-    version = "1.0"
+class HelloConan(ConanFile):
+    name = "packageTemplate"
+    version = "0.1"
     license = "<Put the package license here>"
     url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Testpackage here>"
+    description = "<Description of Hello here>"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
-
+    exports_sources = "src/*"
+    
     def source(self):
         self.run("git clone https://github.com/memsharded/hello.git")
         self.run("cd hello && git checkout static_shared")
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
         # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-                              '''PROJECT(MyHello)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        #tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="hello")
+    def build (self):
+        trunkPath = os.getcwd().replace("\conan",'')
+        cmakePath = trunkPath
+        buildPath = trunkPath + "/build"
+        cmake     = CMake(self)
+        cmake.configure(source_dir=cmakePath, build_dir=buildPath)
         cmake.build()
 
         # Explicit way:
@@ -34,13 +35,12 @@ conan_basic_setup()''')
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
-    def package_info(self):
-        self.cpp_info.libs = ["hello"]
-
+    #def package_info(self):
+        #self.cpp_info.libs = ["hello"]
