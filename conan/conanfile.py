381bfb690786f3c69b07838f9f480b44fd784464
saitama <sitkowskis@interia.pl>
2019-07-14 19:02:45 +0200

    update

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -5,8 +5,8 @@ class HelloConan(ConanFile):
     name = "packageTemplate"
     version = "0.1"
     license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Hello here>"
+    url = "https://github.com/ssitkowx/PackageTemplate"
+    description = "Package template"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
@@ -14,8 +14,8 @@ class HelloConan(ConanFile):
     exports_sources = "src/*"
     
     def source(self):
-        self.run("git clone git@github.com:ssitkowx/Packages.git")
-        #self.run("cd Packages/PackageTemplate")
+        self.run("git clone git@github.com:ssitkowx/PackageTemplate.git")
+        self.run("cd PackageTemplate//conan")
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
         # properly
@@ -25,16 +25,15 @@ class HelloConan(ConanFile):
         #      conan_basic_setup()''')
 
     def build (self):
-        #trunkPath = os.getcwd().replace("\conan",'')
-        trunkPath = os.getcwd()
-        cmakePath = trunkPath + "/Packages/Template"
+        trunkPath = os.getcwd().replace("\conan",'')
+        cmakePath = trunkPath + "/PackageTemplate"
         buildPath = cmakePath + "/build"
         cmake     = CMake(self)
         cmake.configure(source_dir=cmakePath, build_dir=buildPath)
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="src")
+        self.copy("*.h", dst="PackageTemplate/include", src="PackageTemplate/source")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
