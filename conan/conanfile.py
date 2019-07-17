65bbf176f24438b06adc63626cb63ba3f634230d
saitama <sitkowskis@interia.pl>
2019-07-17 22:12:33 +0200

    update

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -25,7 +25,7 @@ class HelloConan(ConanFile):
         #      include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
         #      conan_basic_setup()''')
 
-    def build (self):
+    def build(self):
         cmakePath = os.getcwd().replace("\conan",'')
         buildPath = cmakePath + "/build"
         cmake     = CMake(self)
@@ -33,12 +33,16 @@ class HelloConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="PackageTemplate/include", src="PackageTemplate/source")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        print("zonk!!!!!!!!!!")
+        cmakePath = os.getcwd().replace("\conan",'')
+
+        self.copy("*.h", dst="include", src="../Project/include", keep_path=False)
+        self.copy("*.hxx", dst="include", src="../Project/include", keep_path=False)
+        self.copy("*.lib", dst="lib", src="../build/lib", keep_path=False)
+        self.copy("*.dll", dst="bin", src="../build/bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", src="../build/lib", keep_path=False)
+        self.copy("*.so", dst="lib", src="../build/lib", keep_path=False)
+        self.copy("*.a", dst="lib", src="../build/lib", keep_path=False)
 
     #def package_info(self):
         #self.cpp_info.libs = ["PackageTemplate"]
