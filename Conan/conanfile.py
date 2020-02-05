26374553bb40c660dd6c3274258b3d0fea30af07
sylsit <sitkowskis@interia.pl>
2020-02-05 20:50:22 +0100

    change replace in file path

--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -34,8 +34,8 @@ class Conan(ConanFile):
             cmake.build()
         else:
             raise Exception('Unsupported os in build')
-
-        tools.replace_in_file("..\\CMakeLists.txt", "Template", self.name, False)
+            
+        tools.replace_in_file(projectPath + "\\CMakeLists.txt", "Template", self.name, False)
         
     def package(self):     
         currentPath = os.getcwd();
