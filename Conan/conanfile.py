3a0f63bfc8757649042acd7dc98bdc18c5d2c82c
sylsit <sitkowskis@interia.pl>
2020-04-16 19:03:05 +0200

    conan create package paths update

--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -3,7 +3,7 @@ import os, re
 
 class Conan(ConanFile):
     name            = "Template"
-    #version         = "1.0"
+    version         = "1.0"
     license         = "freeware"
     repoUrl         = "https://github.com/ssitkowx"
     url             = repoUrl + '/' + name + '.git'
@@ -43,15 +43,11 @@ class Conan(ConanFile):
             self.createPackage  (user,channel)
 
     def build(self):
-        projectPath = os.getcwd().replace('\Conan','')
-        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
-            projectPath = os.getcwd() + '\\' + self.name
-
-        self.buildPath = projectPath + '\\Build'
+        projectPath = self.downloadsPath + '\\' + self.name
         
         if self.settings.os == 'Windows' and self.settings.compiler == 'Visual Studio':
             cmake = CMake(self)
-            cmake.configure(source_dir=projectPath, build_dir=self.buildPath)
+            cmake.configure(source_dir=projectPath)
             cmake.build()
         else:
             raise Exception('Unsupported platform or compiler')
@@ -59,17 +55,15 @@ class Conan(ConanFile):
         tools.replace_in_file(projectPath + "\\CMakeLists.txt", "Template", self.name, False)
         
     def package(self):     
-        projectPath = os.getcwd().replace('\Conan','')
-        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
-            projectPath = self.buildPath.replace('Build','')
+        projectPath = self.downloadsPath + '\\' + self.name
     
-        self.copy('*.h'     , dst='include', src= projectPath + '\\' + self.name, keep_path=False)
-        self.copy('*.hxx'   , dst='include', src= projectPath + '\\' + self.name, keep_path=False)
-        self.copy('*.lib'   , dst='lib'    , src= projectPath + '\Build\lib'    , keep_path=False)
-        self.copy('*.dll'   , dst='bin'    , src= projectPath + '\Build\bin'    , keep_path=False)
-        self.copy('*.dylib*', dst='lib'    , src= projectPath + '\Build\lib'    , keep_path=False)
-        self.copy('*.so'    , dst='lib'    , src= projectPath + '\Build\lib'    , keep_path=False)
-        self.copy('*.a'     , dst='lib'    , src= projectPath + '\Build\lib'    , keep_path=False)
+        self.copy('*.h'     , dst='include', src= projectPath + '\\Project' , keep_path=False)
+        self.copy('*.hxx'   , dst='include', src= projectPath + '\\Project' , keep_path=False)
+        self.copy('*.lib'   , dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
+        self.copy('*.dll'   , dst='bin'    , src= projectPath + '\Build\bin', keep_path=False)
+        self.copy('*.dylib*', dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
+        self.copy('*.so'    , dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
+        self.copy('*.a'     , dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = [self.name]
