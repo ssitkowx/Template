81407b9b0e17c1a9396825d311eb4407034e518a
sylsit <sitkowskis@interia.pl>
2020-04-19 13:57:40 +0200

    paths update

--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -14,6 +14,7 @@ class Conan(ConanFile):
     default_options = "shared=False"
     generators      = "cmake"
     author          = "sylsit"
+    exports_sources = '../*'
     requires        = "gtest/1.8.1@bincrafters/stable"
     build_requires  = []
     
@@ -43,19 +44,27 @@ class Conan(ConanFile):
             self.createPackage  (user,channel)
 
     def build(self):
-        projectPath = self.downloadsPath + '\\' + self.name
+        projectPath  = os.getcwd().replace('\Conan','')
+        projectBuild = projectPath + '\\Build'
         
+        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
+            projectPath  = self.downloadsPath + '\\' + self.name
+            projectBuild = os.getcwd() + '\\Build'
+
         if self.settings.os == 'Windows' and self.settings.compiler == 'Visual Studio':
             cmake = CMake(self)
-            cmake.configure(source_dir=projectPath)
+            cmake.configure(source_dir=projectPath, build_dir=projectBuild)
             cmake.build()
         else:
             raise Exception('Unsupported platform or compiler')
             
         tools.replace_in_file(projectPath + "\\CMakeLists.txt", "Template", self.name, False)
         
-    def package(self):     
-        projectPath = self.downloadsPath + '\\' + self.name
+    def package(self):   
+        projectPath = os.getcwd().replace('\Conan','')
+        
+        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
+            projectPath = self.downloadsPath + '\\' + self.name
     
         self.copy('*.h'     , dst='include', src= projectPath + '\\Project' , keep_path=False)
         self.copy('*.hxx'   , dst='include', src= projectPath + '\\Project' , keep_path=False)
