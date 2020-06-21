8874222b93eda99651f4df4d1a5700660c0b7aa4
sylsit <sitkowskis@interia.pl>
2020-06-21 18:20:56 +0200

    replace tabs with spaces. Update conanfile.txt with latest version. Update description. Display sceleton added.

--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -1,75 +1,69 @@
 from conans import ConanFile, CMake, tools
+from ConanPackages import ConanPackages  
 import os, re
 
 class Conan(ConanFile):
-    name            = "Template"
-    #version         = "1.0"
+    name            = "Display"
+    version         = "1.0"
+    user            = "ssitkowx"
+    channel         = "stable"
     license         = "freeware"
     repoUrl         = "https://github.com/ssitkowx"
     url             = repoUrl + '/' + name + '.git'
-    downloadsPath   = "C:/Users/sitko/.conan/download"
-    description     = "Template for projects and packages"
+    description     = "Logs informations in tests"
     settings        = "os", "compiler", "build_type", "arch"
     options         = {"shared": [True, False]}
     default_options = "shared=False"
     generators      = "cmake"
     author          = "sylsit"
-    requires        = "gtest/1.8.1@bincrafters/stable"
-    build_requires  = []
-    
-    def createDownload(self):
-        if not os.path.isdir(self.downloadsPath):
-            os.mkdir(self.downloadsPath)
-        os.chdir(self.downloadsPath)
+    exports         = "*"
+    exports_sources = '../*'
+    requires        = ["gtest/1.8.1@bincrafters/stable"]
+    DownloadsPath   = "C:/Users/sitko/.conan/download"
+    PackagesPath    = "C:/Users/sitko/.conan/data"
+    Packages        = ["Utils/1.0@ssitkowx/stable"]
+
+    def source (self):   
+        ConanPackages.Install (self, self.DownloadsPath, self.repoUrl, self.Packages)
+
+    def build (self):
+        projectPath  = os.getcwd ().replace ('\Conan','')
+        buildPath = projectPath + '\\Build'
         
-    def cloneRepo(self, name):
-        if not os.path.isdir(name):
-            self.run('git clone ' + self.repoUrl + '/' + name + '.git')
-        os.chdir(self.downloadsPath + '/' + name + '/Conan')
-    
-    def createPackage(self, user, channel):
-        self.run('conan create . ' + user + '/' + channel)
-    
-    def source(self):
-        for packages in self.build_requires:
-            package = (re.split('[/@]', packages, 3))
-            name    = package[0]
-            #version = package[1]
-            user    = package[2]
-            channel = package[3]
-
-            self.createDownload () 
-            self.cloneRepo      (name)
-            self.createPackage  (user,channel)
-
-    def build(self):
-        projectPath = os.getcwd().replace('\Conan','')
-        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
-            projectPath = os.getcwd() + '\\' + self.name
-
-        self.buildPath = projectPath + '\\Build'
-        
-        if self.settings.os == 'Windows' and self.settings.compiler == 'Visual Studio':
-            cmake = CMake(self)
-            cmake.configure(source_dir=projectPath, build_dir=self.buildPath)
-            cmake.build()
-        else:
-            raise Exception('Unsupported platform or compiler')
+        if not os.path.exists (projectPath + '\\CMakeLists.txt'):
+            projectPath = self.downloadsPath + '\\' + self.name
+            buildPath   = os.getcwd() + '\\Build'
             
-        tools.replace_in_file(projectPath + "\\CMakeLists.txt", "Template", self.name, False)
-        
-    def package(self):     
-        projectPath = os.getcwd().replace('\Conan','')
-        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
-            projectPath = self.buildPath.replace('Build','')
-    
-        self.copy('*.h'     , dst='include', src= projectPath + '\\' + self.name, keep_path=False)
-        self.copy('*.hxx'   , dst='include', src= projectPath + '\\' + self.name, keep_path=False)
-        self.copy('*.lib'   , dst='lib'    , src= projectPath + '\Build\lib'    , keep_path=False)
-        self.copy('*.dll'   , dst='bin'    , src= projectPath + '\Build\bin'    , keep_path=False)
-        self.copy('*.dylib*', dst='lib'    , src= projectPath + '\Build\lib'    , keep_path=False)
-        self.copy('*.so'    , dst='lib'    , src= projectPath + '\Build\lib'    , keep_path=False)
-        self.copy('*.a'     , dst='lib'    , src= projectPath + '\Build\lib'    , keep_path=False)
+        tools.replace_in_file (projectPath + "\\CMakeLists.txt", "PackageTempName", self.name, False)
 
-    def package_info(self):
+        if self.settings.os == 'Windows' and self.settings.compiler == 'Visual Studio':
+            packagesPaths = ConanPackages.GetPaths (self, self.PackagesPath, self.Packages)
+            cmake         = CMake(self)
+            
+            conanPath = os.getcwd () + "\\PackagesProperties.txt"
+            PackagesPropertiesFileHandler = open (conanPath, "w")
+            for packagePathKey, packagePathValue in packagesPaths.items ():
+                PackagesPropertiesFileHandler.writelines (packagePathKey + "=" + packagePathValue + "\n")
+            PackagesPropertiesFileHandler.close ()
+
+            cmake.configure (source_dir = projectPath, build_dir = buildPath)
+            cmake.build ()
+        else:
+            raise Exception ('Unsupported platform or compiler')
+        
+    def package (self):   
+        projectPath = os.getcwd ().replace ('\Conan','')
+        
+        if not os.path.exists (projectPath + '\\CMakeLists.txt'):
+            projectPath = self.downloadsPath + '\\' + self.name
+    
+        self.copy ('*.h'     , dst = 'include', src = projectPath + '\\Project' , keep_path = False)
+        self.copy ('*.hxx'   , dst = 'include', src = projectPath + '\\Project' , keep_path = False)
+        self.copy ('*.lib'   , dst = 'lib'    , src = projectPath + '\Build\lib', keep_path = False)
+        self.copy ('*.dll'   , dst = 'bin'    , src = projectPath + '\Build\bin', keep_path = False)
+        self.copy ('*.dylib*', dst = 'lib'    , src = projectPath + '\Build\lib', keep_path = False)
+        self.copy ('*.so'    , dst = 'lib'    , src = projectPath + '\Build\lib', keep_path = False)
+        self.copy ('*.a'     , dst = 'lib'    , src = projectPath + '\Build\lib', keep_path = False)
+
+    def package_info (self):
         self.cpp_info.libs = [self.name]
--- README.md
+++ README.md
@@ -1,16 +1,29 @@
-# Template
-Template for projects and packages
+# Display
+General class for display.
 
 Configuration:
-1. GTest and GMock tooked from 
+1. Currently used:
+- Python 2.7.16,
+- cmake version 3.17.0-rc2,
+- Visual Studio 2019.
+
+2. GTest and GMock tooked from:
    https://bintray.com/bincrafters/public-conan/gtest%3Abincrafters/1.8.1%3Astable#
 
-2. You need update your remotes.
-   conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+3. You need update your remotes:
+   Conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
 Builidng:
 1. Go to Conan folder,
-2. Open console,
+2. Open git bash console,
 3. Type conan source .
 4. Type conan install .
 5. Type conan build .
+6. Got to "Build" folder and open Visual Project.
+
+Tips:
+- It is unacceptable if the package calls the package (recursion),
+- The first time you start the program python packages can be missing. Please follow the python suggestion to install them,
+- The first time you start the program using "conan build ." CMakeLists is updated with current project name and packages names. 
+  Remove Build folder and try again with updated CMakeLists.txt,
+- To install gtest and gmock packages for specified options and settings type "conan install . --build gtest".
